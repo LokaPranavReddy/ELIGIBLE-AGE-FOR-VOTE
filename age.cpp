8a9b371ecc8caa31efaed7890b123a0eeb7c6c0b
LokaPranavReddy <78609056+LokaPranavReddy@users.noreply.github.com>
2021-04-18 11:56:07 +0530

    Create age.cpp

---
+++ age.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"Enter Age of a user:";
+    cin>>age;
+    if(age>=18)
+        cout<<"\n You are eligible";
+    else 
+        cout<<"\n You are not eligible";
+return 0;
+}
